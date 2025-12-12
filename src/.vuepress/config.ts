7114854c5a2ff4b7077fc3686aea2c38598b9957
zy <56502471+zhangyao1999@users.noreply.github.com>
2025-12-12 23:38:14 +0800

    111

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,6 @@
 import { defineUserConfig } from "vuepress";
 
 import theme from "./theme.js";
-import { searchPlugin } from '@vuepress/plugin-search'
 
 export default defineUserConfig({
   base: "/",
@@ -12,15 +11,6 @@ export default defineUserConfig({
 
   theme,
 
-  // plugins: [
-  //   searchPlugin({
-  //     // 配置项
-  //     maxSuggestions: 10,
-  //     getExtraFields: (page) => page.frontmatter.tags ?? [],
-  //   }),
-  // ],
-
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
-
